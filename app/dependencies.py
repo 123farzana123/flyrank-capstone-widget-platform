71226d40f1dc6f879a512869385a585157bd9b3b
Farzana <ff.yaqubi@gmail.com>
2026-08-27 10:58:42 -0700

    Wire up submission service in dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,15 @@
 from .repositories.widget_repository import WidgetRepository
 from .services.widget_service import WidgetService
+from .services.submission_service import SubmissionService
+from .repositories.submission_repository import SubmissionRepository
 
 _repository = WidgetRepository()
 _service = WidgetService(_repository)
+_submission_repository = SubmissionRepository()
+_submission_service = SubmissionService(_submission_repository)
 
 def get_service() -> WidgetService:
     return _service
+
+def get_submission_service() -> SubmissionService:
+    return _submission_service
